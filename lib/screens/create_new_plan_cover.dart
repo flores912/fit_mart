71d0bd79a78d5a3127bc44f57ca90f703e68869f
flores912 <47996013+flores912@users.noreply.github.com>
2020-11-22 13:02:29 -0700

    -added crop feature for cover photo

--- lib/screens/create_new_plan_cover.dart
+++ lib/screens/create_new_plan_cover.dart
@@ -6,6 +6,7 @@ import 'package:fit_mart/widgets/workout_card_widget.dart';
 import 'package:fit_mart/widgets/workout_plan_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
 import 'create_new_plan_pricing.dart';
@@ -21,14 +22,32 @@ class CreateNewPlanCoverScreen extends StatefulWidget {
 }
 
 class CreateNewPlanCoverScreenState extends State<CreateNewPlanCoverScreen> {
-  PickedFile imageUri;
-  final ImagePicker _picker = ImagePicker();
+  File _pickedImage;
+  final picker = ImagePicker();
 
-  Future getImageFromCameraGallery(bool isCamera) async {
-    var image = await _picker.getImage(
+  File _croppedImage;
+
+  Future getImage(bool isCamera) async {
+    final pickedFile = await picker.getImage(
         source: (isCamera == true) ? ImageSource.camera : ImageSource.gallery);
+    cropImage(pickedFile.path);
+  }
+
+  Future cropImage(String imagePath) async {
+    _pickedImage = await ImageCropper.cropImage(
+        sourcePath: imagePath,
+        aspectRatioPresets: [CropAspectRatioPreset.ratio16x9],
+        androidUiSettings: AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: kPrimaryColor,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false),
+        iosUiSettings: IOSUiSettings(
+          minimumAspectRatio: 1.0,
+        ));
     setState(() {
-      imageUri = image;
+      _croppedImage = _pickedImage;
     });
   }
 
@@ -52,51 +71,55 @@ class CreateNewPlanCoverScreenState extends State<CreateNewPlanCoverScreen> {
         ],
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(32.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             WorkoutPlanCardWidget(
-              imageUri: imageUri,
+              image: _croppedImage,
             ),
             SizedBox(
               height: 16,
             ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: RaisedButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                color: kPrimaryColor,
-                child: Text(
-                  'Add From Gallery',
-                  style: TextStyle(
-                    color: Colors.white,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    color: kPrimaryColor,
+                    child: Text(
+                      'Add From Gallery',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      getImage(false);
+                    },
                   ),
                 ),
-                onPressed: () {
-                  getImageFromCameraGallery(false);
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(4.0),
-              child: RaisedButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                color: kPrimaryColor,
-                child: Text(
-                  'Take Photo',
-                  style: TextStyle(
-                    color: Colors.white,
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    color: kPrimaryColor,
+                    child: Text(
+                      'Take Photo',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      getImage(true);
+                    },
                   ),
                 ),
-                onPressed: () {
-                  getImageFromCameraGallery(true);
-                },
-              ),
+              ],
             ),
           ],
         ),
--- lib/widgets/workout_plan_card.dart
+++ lib/widgets/workout_plan_card.dart
@@ -1,106 +1,106 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:smooth_star_rating/smooth_star_rating.dart';
 
 class WorkoutPlanCardWidget extends StatelessWidget {
-  final PickedFile imageUri;
+  final File image;
 
   const WorkoutPlanCardWidget({
     Key key,
-    this.imageUri,
+    this.image,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Wrap(
-      children: [
-        Card(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 150,
-                  width: MediaQuery.of(context).size.width,
-                  color: Colors.grey,
-                  child: imageUri == null
-                      ? Icon(
+    return Container(
+      width: MediaQuery.of(context).size.width / 1.5,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                color: Colors.grey,
+                child: image == null
+                    ? Container(
+                        height: MediaQuery.of(context).size.width / 1.5 / 1.78,
+                        child: Icon(
                           Icons.image,
                           color: Colors.grey.shade800,
                           size: 100,
-                        )
-                      : Container(
-                          height: 150,
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: AssetImage(imageUri.path),
-                            ),
-                          ),
                         ),
-                ),
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 8, 8, 8),
-                      child: Text(
-                        'Bodybuilding',
+                      )
+                    : Container(
+                        height: MediaQuery.of(context).size.width / 1.5 / 1.78,
+                        child: Image.file(
+                          image,
+                          fit: BoxFit.fill,
+                        ),
                       ),
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 8, 8, 8),
+                    child: Text(
+                      'Bodybuilding',
                     ),
-                    Icon(
-                      CupertinoIcons.circle_fill,
-                      size: 8,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text('Gym'),
-                    ),
-                    Icon(
-                      CupertinoIcons.circle_fill,
-                      size: 8,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text('Advanced'),
-                    ),
-                  ],
-                ),
-                Text(
-                  "The Rock's Ultimate Mass Builder Plan",
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.normal,
                   ),
-                ),
-                Row(
-                  children: [
-                    SmoothStarRating(
-                      rating: 5,
-                    ),
-                    Text(
-                      '5.0(939)',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    )
-                  ],
-                ),
-                Text(
-                  '\$9.99',
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
+                  Icon(
+                    CupertinoIcons.circle_fill,
+                    size: 8,
                   ),
-                )
-              ],
-            ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Gym'),
+                  ),
+                  Icon(
+                    CupertinoIcons.circle_fill,
+                    size: 8,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Advanced'),
+                  ),
+                ],
+              ),
+              Text(
+                "The Rock's Ultimate Mass Builder Plan",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              Row(
+                children: [
+                  SmoothStarRating(
+                    rating: 5,
+                  ),
+                  Text(
+                    '5.0(939)',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  )
+                ],
+              ),
+              Text(
+                '\$9.99',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
           ),
         ),
-      ],
+      ),
     );
   }
 }
